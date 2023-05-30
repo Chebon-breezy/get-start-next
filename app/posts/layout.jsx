32de3ad33ca6f90453725b441a26564834c1a7ca
Brian Chebon <brynnlamura@gmail.com>
2023-05-30 12:49:22 +0300

    Added Pages folder n contents

---
+++ app/posts/layout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function layout() {
+  return (
+    <div>Navigate To Top</div>
+  )
+}
+
+export default layout
---
+++ app/posts/loading.jsx
@@ -0,0 +1,6 @@
+const Loading = () => {
+    // you can add any UI inside, including skeleton
+    return <LoadingSkeleton />
+}
+
+export default Loading;
